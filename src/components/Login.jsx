ad86cad55a578a4ec18833a6452755c8e98ece4f
james etchberger <etchbergerjames@gmail.com>
2022-08-11 14:58:07 -0400

    used context and made a fake login and logout button focused on using context

---
+++ src/components/Login.jsx
@@ -0,0 +1,11 @@
+import { useContext } from 'react'
+import { UserInfoContext } from '../App'
+
+export default function Login() {
+    const { setIsLoggedIn, setUser } = useContext(UserInfoContext)
+    const handleLogin = () => {
+        setIsLoggedIn(true)
+        setUser({name: 'Jonathan', position: 'Drums', band: 'Lane Branden'})
+    }
+  return <button onClick={handleLogin}>Login</button>
+}
